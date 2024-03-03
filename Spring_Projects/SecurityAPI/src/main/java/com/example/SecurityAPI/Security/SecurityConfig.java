db85486b27a3dba8b346f9d0ebf8071a18996fac
ToroDDT <142459893+ToroDDT@users.noreply.github.com>
2024-03-02 20:10:04 -0800

    Create Security configuration package

---
+++ Spring_Projects/SecurityAPI/src/main/java/com/example/SecurityAPI/Security/SecurityConfig.java
@@ -0,0 +1,30 @@
+package com.example.SecurityAPI.Security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((authorize) -> authorize
+                .anyRequest().authenticated()
+        ).httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+    @Bean
+    public AuthenticationManager authenticationManager (UserDetailsService userDetailsService){
+
+    }
+}
