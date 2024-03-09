5625964a49a40d0a6deef3f0293ae92facffcb51
ToroDDT <142459893+ToroDDT@users.noreply.github.com>
2024-03-08 21:19:29 -0800

    Search database for username and password
    Whenever a request is sent. Each request will be processed by the security filter chain. This chain will intercept all requests and search the database with jdbc. If authenticated it will allow the application to continue.

---
+++ Spring_Projects/SpringBootSecurityAPI/src/main/java/com/example/SpringBootSecurityAPI/SecurityConfig/SpringSecurityConfig.java
@@ -0,0 +1,41 @@
+package com.example.SpringBootSecurityAPI.SecurityConfig;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationEntryPoint;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class SpringSecurityConfig {
+    @Autowired
+    private DataSource dataSource;
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+        http
+                .authorizeHttpRequests((authorize) -> authorize
+                        .anyRequest().authenticated()).httpBasic(Customizer.withDefaults());
+           return http.build();
+    }
+    @Autowired
+    public void configAuth(AuthenticationManagerBuilder auth) throws Exception{
+        auth.jdbcAuthentication().passwordEncoder(new BCryptPasswordEncoder())
+                .dataSource(dataSource)
+                .usersByUsernameQuery("select username, password, enabled from users where username=?")
+                .authoritiesByUsernameQuery("select username, role from users where username=?");
+    }
+}
