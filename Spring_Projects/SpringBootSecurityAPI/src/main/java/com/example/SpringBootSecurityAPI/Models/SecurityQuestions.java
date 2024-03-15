dffe2ce4d6c1200efe7d99415c428f0c52f49ffd
ToroDDT <142459893+ToroDDT@users.noreply.github.com>
2024-03-14 17:58:56 -0700

    Add security answers and change naming of variables

--- Spring_Projects/SpringBootSecurityAPI/src/main/java/com/example/SpringBootSecurityAPI/Models/SecurityQuestions.java
+++ Spring_Projects/SpringBootSecurityAPI/src/main/java/com/example/SpringBootSecurityAPI/Models/SecurityQuestions.java
@@ -1,36 +1,71 @@
 package com.example.SpringBootSecurityAPI.Models;
 
 public class SecurityQuestions {
-    private String questionOne;
-    private String questionTwo;
-    private String questionThree;
-    public SecurityQuestions(String questionOne, String questionTwo, String questionThree) {
-        this.questionOne = questionOne;
-        this.questionTwo = questionTwo;
-        this.questionThree = questionThree;
-    }
-    public String getQuestionOne() {
-        return questionOne;
+    private String security_question_1;
+    private String security_answer_1;
+    private String security_question_2;
+    private String security_answer_2;
+    private String security_question_3;
+    private String security_answer_3;
+
+    public SecurityQuestions(String security_question_1, String security_answer_1, String security_question_2, String security_answer_2, String security_question_3, String security_answer_3) {
+        this.security_question_1 = security_question_1;
+        this.security_answer_1 = security_answer_1;
+        this.security_question_2 = security_question_2;
+        this.security_answer_2 = security_answer_2;
+        this.security_question_3 = security_question_3;
+        this.security_answer_3 = security_answer_3;
     }
 
-    public void setQuestionOne(String questionOne) {
-        this.questionOne = questionOne;
+
+    public String getSecurity_question_1() {
+        return security_question_1;
     }
 
-    public String getQuestionTwo() {
-        return questionTwo;
+    public void setSecurity_question_1(String security_question_1) {
+        this.security_question_1 = security_question_1;
     }
 
-    public void setQuestionTwo(String questionTwo) {
-        this.questionTwo = questionTwo;
+    public String getSecurity_answer_1() {
+        return security_answer_1;
     }
 
-    public String getQuestionThree() {
-        return questionThree;
+    public void setSecurity_answer_1(String security_answer_1) {
+        this.security_answer_1 = security_answer_1;
     }
 
-    public void setQuestionThree(String questionThree) {
-        this.questionThree = questionThree;
+    public String getSecurity_question_2() {
+        return security_question_2;
     }
+
+    public void setSecurity_question_2(String security_question_2) {
+        this.security_question_2 = security_question_2;
+    }
+
+    public String getSecurity_answer_2() {
+        return security_answer_2;
+    }
+
+    public void setSecurity_answer_2(String security_answer_2) {
+        this.security_answer_2 = security_answer_2;
+    }
+
+    public String getSecurity_question_3() {
+        return security_question_3;
+    }
+
+    public void setSecurity_question_3(String security_question_3) {
+        this.security_question_3 = security_question_3;
+    }
+
+    public String getSecurity_answer_3() {
+        return security_answer_3;
+    }
+
+    public void setSecurity_answer_3(String security_answer_3) {
+        this.security_answer_3 = security_answer_3;
+    }
+
+
 
 }
