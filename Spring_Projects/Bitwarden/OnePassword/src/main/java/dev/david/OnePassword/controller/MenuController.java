8099301f11b248495bd650a6cda32f906cea8d5e
ToroDDT <142459893+ToroDDT@users.noreply.github.com>
2024-08-21 14:49:04 -0700

    Create controller for the generator page

--- Spring_Projects/Bitwarden/OnePassword/src/main/java/dev/david/OnePassword/controller/MenuController.java
+++ Spring_Projects/Bitwarden/OnePassword/src/main/java/dev/david/OnePassword/controller/MenuController.java
@@ -1,23 +1,25 @@
 package dev.david.OnePassword.controller;
 
 import dev.david.OnePassword.form_data.PasswordFormData;
+import dev.david.OnePassword.passwordGenerator.PasswordGenTool;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.Random;
 
 @Controller
 public class MenuController {
     @PostMapping("/tools/generator")
-    public String displayGeneration(Model model, @RequestBody PasswordFormData passwordFormData) {
-
+    public String displayGeneration(@ModelAttribute PasswordFormData passwordFormData, Model model) {
+        PasswordGenTool newPassword = new PasswordGenTool();
+        newPassword.generateRandomPassword(passwordFormData);
+        model.addAttribute("newPassword", newPassword.getPassword());
         return "generator";
     }
-    @RequestMapping("/tools/generator")
+    @GetMapping("/tools/generator")
     public String generatorTool(Model model) {
-        model.addAttribute("generatedPassword", "HELOJOJDJF");
+        model.addAttribute("passwordFormData", new PasswordFormData());
         return "generator";
     }
 
