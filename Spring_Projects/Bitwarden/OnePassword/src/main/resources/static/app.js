2fce22e0b0a48df0cc0c7779120e97a9131208a5
ToroDDT <142459893+ToroDDT@users.noreply.github.com>
2024-08-21 14:47:32 -0700

    Connect to websocket and display messages from subscription

---
+++ Spring_Projects/Bitwarden/OnePassword/src/main/resources/static/app.js
@@ -0,0 +1,61 @@
+const stompClient = new StompJs.Client({
+    brokerURL: 'ws://localhost:8080/websocket'
+});
+
+stompClient.onConnect = (frame) => {
+    setConnected(true);
+    console.log('Connected: ' + frame);
+    stompClient.subscribe('/tool/messaging', (message) => {
+        let newMessage = JSON.parse(message.body).message;
+        showMessage(newMessage);
+    });
+};
+
+stompClient.onWebSocketError = (error) => {
+    console.error('Error with websocket', error);
+};
+
+stompClient.onStompError = (frame) => {
+    console.error('Broker reported error: ' + frame.headers['message']);
+    console.error('Additional details: ' + frame.body);
+};
+
+function setConnected(connected) {
+    $("#connect").prop("disabled", connected);
+    $("#disconnect").prop("disabled", !connected);
+    if (connected) {
+        $("#conversation").show();
+    }
+    else {
+        $("#conversation").hide();
+    }
+    $("#greetings").html("");
+}
+
+function connect() {
+    stompClient.activate();
+}
+
+function disconnect() {
+    stompClient.deactivate();
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function sendMessage() {
+    stompClient.publish({
+        destination: "/app/messages",
+        body: JSON.stringify({'message': $("#message").val()})
+    });
+}
+
+function showMessage(message) {
+    $("#messages").append("<div><div>" + message + "</div></div>");
+}
+
+$(function () {
+    $("form").on('submit', (e) => e.preventDefault());
+    $( "#connect" ).click(() => connect());
+    $( "#disconnect" ).click(() => disconnect());
+    $( "#send" ).click(() => sendMessage());
+});
